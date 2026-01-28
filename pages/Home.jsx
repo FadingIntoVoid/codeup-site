e5574dcb9a7a3c4821f14b5cca0ea5a12acc333a
FadingIntoVoid <lucasgabpsilva@gmail.com>
2026-01-28 10:19:30 -0300

    Add files via upload

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,8 +1,8 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
-import CTA from "../components/Cta";
-import Contact from "../components/Contact"
+import CTA from "../components/CTA";
+import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 function Home() {
